44c7464ba34b93978d2b6f2c8cf308c3a64743f0
Nazar-de-github <derkevich.n@gmail.com>
2025-10-05 16:23:13 +0300

    Create model

---
+++ train.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from model import train_model, evaluate_model
+from metrics_and_plots import save_metrics
+
+data = pd.read_csv("data.csv")
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+
+X = pd.get_dummies(data[features])
+y = data["Survived"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
+
+model = train_model(X_train, y_train)
+
+metrics = evaluate_model(model, X_test, y_test)
+save_metrics(metrics)
+
+print(metrics)
+
+
